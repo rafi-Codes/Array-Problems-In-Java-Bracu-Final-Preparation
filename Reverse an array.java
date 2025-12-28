2796ee8f70b4222274278f79b0795b4529e18fb2
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-12-28 11:27:39 +0600

    Create Reverse an array.java

---
+++ Reverse an array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class codeforces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+
+        sc.close();
+    }
+}
